46e526c84822cca2deb118194e236ec506d3dc27
Joe <joseph.lee3454@gmail.com>
2021-02-11 17:53:11 -0800

    added a new class

--- wizard.cs
+++ wizard.cs
@@ -1,1 +1,9 @@
 ////ewrgiuerngiuwnrginwreiugn4rg
+public int Attack(Human target)
+    {
+        int dmg = Strength * 3;
+        target.health -= dmg;
+        Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+        return target.health;
+    }
+}
